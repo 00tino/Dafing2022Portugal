e69c63297f567894a2c61a6c565c60848bb74de4
00tino <fonteviva.valen.tino00@gmail.com>
2022-10-20 15:15:20 +0200

    infocards open and close

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,63 @@
         navbar.classList.toggle('open');
     });
 })()
+
+function firstFunction() {
+    var x = document.getElementById("firstInfoCard");
+    var y = document.getElementById("firstInfoCard");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      y.style.display = "none";
+    }
+  }
+
+  function secondFunction() {
+    var x = document.getElementById("secondInfoCard");
+    var y = document.getElementById("secondInfoCard");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      y.style.display = "none";
+    }
+  }
+
+  function thirdFunction() {
+    var x = document.getElementById("thirdInfoCard");
+    var y = document.getElementById("thirdInfoCard");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      y.style.display = "none";
+    }
+  }
+
+  function fourthFunction() {
+    var x = document.getElementById("fourthInfoCard");
+    var y = document.getElementById("fourthInfoCard");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      y.style.display = "none";
+    }
+  }
+
+  function fifthFunction() {
+    var x = document.getElementById("fifthInfoCard");
+    var y = document.getElementById("fifthInfoCard");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      y.style.display = "none";
+    }
+  }
+
+  function sixthFunction() {
+    var x = document.getElementById("sixthInfoCard");
+    var y = document.getElementById("sixthInfoCard");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      y.style.display = "none";
+    }
+  }
