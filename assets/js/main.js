2e4d382bbc73a077e97c8e8c3c6418de732c6a7e
00tino <fonteviva.valen.tino00@gmail.com>
2022-10-23 17:27:06 +0200

    upgrade center Ubicacion and staff

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,15 +72,15 @@ $(".hover").mouseleave(
     }
 );
 
-window.addEventListener("load", function(){
-    setTimeout(
-        function open(event){
-            document.querySelector(".popup").style.display = "block";
-        },
-        1000 
-    )
-});
+// window.addEventListener("load", function(){
+//     setTimeout(
+//         function open(event){
+//             document.querySelector(".popup").style.display = "block";
+//         },
+//         1000 
+//     )
+// });
 
-document.querySelector("#close").addEventListener("click", function(){
-    document.querySelector(".popup").style.display = "none";
-});
+// document.querySelector("#close").addEventListener("click", function(){
+//     document.querySelector(".popup").style.display = "none";
+// });
